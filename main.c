77f3f9b09f07f183e49f3334cfa957d547ab8dbf
achaika01 <achaika@kse.org.ua>
2025-05-11 14:12:46 +0300

    changed to dynamic array

--- main.c
+++ main.c
@@ -1,16 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define ROWS 10
-#define COLUMS 10
-
-char text[ROWS][COLUMS] = { 0 };
-void command(int input);
 void append_symbols_end();
 void resize_text();
+int get_free_space_in_arr();
+void init_text();
+void command(int input);
+
+int rows = 10;
+int colums = 10;
+
+char** text = NULL;
 
 int main(int argc, char* argv[]) {
     printf("Commands:\n1.Append text symbols to the end\n2.Start the new line\n3.Use files to load the information\n4.Use files to save the information\n5.Print the current text to console\n6.Insert the text by line and symbol index\n7.Search\n8.End\n");
     int user_command;
+    init_text();
     do {
         printf("Enter command: ");
         scanf_s("%d", &user_command);
@@ -19,9 +23,6 @@ int main(int argc, char* argv[]) {
         while ((c = getchar()) != '\n' && c != EOF);
 
         command(user_command);
-        if (user_command == 1) {
-            print_text();
-        }
     } while (user_command != 8);
 
 
@@ -72,17 +73,19 @@ void append_symbols_end() {
             text_has_right_size = 1;
         }
     }
+    if (strlen(text_to_app) > get_free_space_in_arr()) {
+        resize_text();
+    }
     char* poiner = &text[0][0];
-    int total_ar_size = ROWS * COLUMS;
     int num = 0;
+    int total_ar_size = rows * colums;
     while (num < total_ar_size && poiner[num] != '\0') {
         num++;
     }
-    int first_free_row = num / COLUMS;
-    int first_free_col = num - (first_free_row * COLUMS);
+    int first_free_row = num / colums;
+    int first_free_col = num - (first_free_row * colums);
 
-    for (int i = 0; i <= total_ar_size; i++) {
-        if (num + i >= total_ar_size) break;
+    for (int i = 0; text_to_app_ptr[i] != '\0' && num + i < total_ar_size; i++) {
         poiner[num + i] = text_to_app_ptr[i];
     }
 
@@ -93,7 +96,42 @@ void append_symbols_end() {
 }
 
 void resize_text() {
-
+    int new_rows = 2 * rows;
+    int new_colums = 2 * colums;
+    char** new_text = malloc(new_rows * sizeof(char*));
+    for (int i = 0; i < new_rows; i++) {
+        new_text[i] = calloc(new_colums, sizeof(char));
+    }
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < colums; j++) {
+            new_text[i][j] = text[i][j];
+        }
+    }
+    for (int i = 0; i < rows; i++) {
+        free(text[i]);
+    }
+    free(text);
+    text = new_text;
+    rows = new_rows;
+    colums = new_colums;
+}
+int get_free_space_in_arr() {
+    int count = 0;
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < colums; j++) {
+            if (text[i][j] != '\0') {
+                count++;
+            }
+            
+        }
+    }
+    return rows*colums - count;
+}
+void init_text() {
+    text = malloc(rows * sizeof(char*));
+    for (int i = 0; i < rows; i++) {
+        text[i] = calloc(colums, sizeof(char));
+    }
 }
 
 
